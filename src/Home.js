c02e2f94dd118c0096b13a2281467ebd755d9674
enztexier <enzotexier.studio@gmail.com>
2023-03-24 23:21:55 +0100

    checkbox operator

--- src/Home.js
+++ src/Home.js
@@ -2,23 +2,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css'
 import Checkbox from './Checkbox';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Home = () => {
 
-    const [checkedOne, setCheckedOne] = useState("heyyyy");
-    const [checkedTwo, setCheckedTwo] = useState(false);
+    const [checkedPositive, setCheckedPositive] = useState(false);
+    const [checkedNegative, setCheckedNegative] = useState(false);
+    const [checkedDivision, setCheckedDivision] = useState(false);
+    const [checkedMultiplication, setCheckedMultiplication] = useState(false);
+    const [operator, setOperator] = useState([]);
+    const [calcul, setcalcul] = useState(false);
 
     const handleChangeOne = () => {
-        setCheckedOne(!checkedOne);
-        console.log('checkbooxOne');
-        console.log(checkedOne);
+        setCheckedPositive(!checkedPositive);
     };
 
     const handleChangeTwo = () => {
-        setCheckedTwo(!checkedTwo);
-        console.log('checkboxTwo');
-        console.log(checkedTwo);
+        setCheckedNegative(!checkedNegative);
+    };
+
+    const handleChangeThree = () => {
+        setCheckedDivision(!checkedDivision);
+    };
+
+    const handleChangeFour = () => {
+        setCheckedMultiplication(!checkedMultiplication);
     };
 
     const GetRandomInt = () => {
@@ -26,13 +34,77 @@ const Home = () => {
         let two = Math.floor(Math.random() * 99);
         let result = one + two;
         let b = <p>{one} + {two} = {result}</p>;
-        console.log(b);
-        let a = "yoooo";
-        console.log(a);
-        console.log(checkedOne);
-        setCheckedOne(b);
+        setcalcul(b);
+        console.log(operator);
     }
 
+    useEffect(() => {
+
+        if (checkedPositive === true) {
+            operator.push('+');
+            console.log(operator);
+        }
+        else {
+            const index = operator.indexOf('+');
+            if (index > -1) { // only splice array when item is found
+                operator.splice(index, 1);
+                console.log(operator);
+            }
+        }
+
+    },[checkedPositive]);
+
+
+    useEffect(() => {
+
+        if (checkedNegative === true) {
+            operator.push('-');
+            console.log(operator);
+        }
+        else {
+            const index = operator.indexOf('-');
+            if (index > -1) {
+                operator.splice(index, 1);
+                console.log(operator);
+
+            }
+        }
+
+    },[checkedNegative]);
+
+    useEffect(() => {
+
+        if (checkedDivision === true) {
+            operator.push('/');
+            console.log(operator);
+        }
+        else {
+            const index = operator.indexOf('/');
+            if (index > -1) {
+                operator.splice(index, 1);
+                console.log(operator);
+
+            }
+        }
+
+    },[checkedDivision]);
+
+    useEffect(() => {
+
+        if (checkedMultiplication === true) {
+            operator.push('*');
+            console.log(operator);
+        }
+        else {
+            const index = operator.indexOf('*');
+            if (index > -1) {
+                operator.splice(index, 1);
+                console.log(operator);
+
+            }
+        }
+
+    },[checkedMultiplication]);
 
     return (
         <div>
@@ -40,21 +112,32 @@ const Home = () => {
             <Link to="/copyright">Copyright</Link>
             <br></br>
             <Checkbox
-                label="Value 1"
-                value={checkedOne}
+                label="+"
+                value={checkedPositive}
                 onChange={handleChangeOne}
             />
 
             <Checkbox
-                label="Value 2"
-                value={checkedTwo}
+                label="-"
+                value={checkedNegative}
                 onChange={handleChangeTwo}
             />
 
-            <p>Is "My Value" checked? {checkedOne}</p>
+            <Checkbox
+                label="/"
+                value={checkedDivision}
+                onChange={handleChangeThree}
+            />
+
+            <Checkbox
+                label="*"
+                value={checkedMultiplication}
+                onChange={handleChangeFour}
+            />
+
 
             <button onClick={GetRandomInt}>click</button>
-
+            {calcul}
 
 
         </div>
